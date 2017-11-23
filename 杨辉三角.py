a2ec73052017d3ea04df28659c0f92c017902477
Loeve <1872197055@qq.com>
2017-11-23 10:57:31 +0800

    Update 杨辉三角.py

--- 杨辉三角.py
+++ 杨辉三角.py
@@ -1,3 +1,4 @@
+#不用生成器
 def pascal_triangle(line):
     i, arr = 0, [1]
     for n in range(line):
@@ -6,3 +7,20 @@ def pascal_triangle(line):
     return arr
 
 pascal_triangle(10)
+
+#生成器
+def pascal_triangle(line):
+    i, arr = 0, [1]
+    for n in range(line):
+        yield (arr)
+        arr = [1] + [arr[i] + arr[i+1] for i in range(0, len(arr) - 1)] + [1]
+    return 'over'
+#测试
+g = pascal_triangle(10)
+while True:
+    try:
+        x = next(g)
+        print("g:", x)
+    except StopIteration as e:
+        print("Generator return value", e.value)
+        break
